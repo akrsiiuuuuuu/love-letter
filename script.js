86bb57d11e44c9cb46e6419efb6df1adb148b648
akrsiiuuuuuu <ada.10092011@gmail.com>
2026-06-22 16:18:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
+let score = 0;
+
+
+
+function start(){
+
+document.getElementById("welcome").hidden=true;
+
+document.getElementById("game").hidden=false;
+
+spawnHearts();
+
+}
+
+
+
 function spawnHearts(){
 
 const container=document.getElementById("hearts");
 
+
+
 for(let i=0;i<14;i++){
+
 
 let heart=document.createElement("div");
 
 heart.innerHTML="💗";
+
 heart.className="heart";
+
 
 heart.style.left=Math.random()*90+"vw";
 
+
 heart.style.animationDuration=
 (Math.random()*3+3)+"s";
+
+
 
 setTimeout(()=>{
 
@@ -21,32 +45,70 @@ container.appendChild(heart);
 },i*500);
 
 
+
+
 heart.onclick=function(){
+
 
 score++;
 
-document.getElementById("score")
-.innerHTML=
+document.getElementById("score").innerHTML=
+
 `Collected Hearts : ${score}/14`;
+
+
 
 heart.remove();
 
+
+
+
 if(score===14){
 
+
 document.getElementById("game").hidden=true;
+
+
 document.getElementById("loading").hidden=false;
+
+
 
 setTimeout(()=>{
 
+
 document.getElementById("loading").hidden=true;
+
+
 document.getElementById("letter").hidden=false;
+
 
 },3000);
 
-}
 
 }
 
+
 }
+
+
+}
+
+
+}
+
+
+
+
+function openLetter(){
+
+document.getElementById("popup").style.display="block";
+
+}
+
+
+
+function closeLetter(){
+
+document.getElementById("popup").style.display="none";
 
 }
