da50317c71bc05c15ab9de26bff1bd5abf1edf5c
akrsiiuuuuuu <ada.10092011@gmail.com>
2026-06-22 16:21:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,114 +1,66 @@
 let score = 0;
 
+function start() {
+    document.getElementById("welcome").hidden = true;
+    document.getElementById("game").hidden = false;
+
+    spawnHearts();
+}
+
+function spawnHearts() {
+
+    const container = document.getElementById("hearts");
+
+    for (let i = 0; i < 14; i++) {
+
+        let heart = document.createElement("div");
+
+        heart.innerHTML = "💗";
+        heart.className = "heart";
+
+        heart.style.left = Math.random() * 90 + "vw";
+
+        heart.style.animationDuration =
+            (Math.random() * 3 + 3) + "s";
+
+        setTimeout(() => {
+            container.appendChild(heart);
+        }, i * 500);
 
 
-function start(){
+        heart.onclick = function () {
 
-document.getElementById("welcome").hidden=true;
+            score++;
 
-document.getElementById("game").hidden=false;
+            document.getElementById("score").innerHTML =
+                `Collected Hearts : ${score}/14`;
 
-spawnHearts();
+            heart.remove();
+
+            if (score === 14) {
+
+                document.getElementById("game").hidden = true;
+                document.getElementById("loading").hidden = false;
+
+                setTimeout(() => {
+
+                    document.getElementById("loading").hidden = true;
+                    document.getElementById("letter").hidden = false;
+
+                }, 3000);
+
+            }
+
+        };
+
+    }
 
 }
 
-
-
-function spawnHearts(){
-
-const container=document.getElementById("hearts");
-
-
-
-for(let i=0;i<14;i++){
-
-
-let heart=document.createElement("div");
-
-heart.innerHTML="💗";
-
-heart.className="heart";
-
-
-heart.style.left=Math.random()*90+"vw";
-
-
-heart.style.animationDuration=
-(Math.random()*3+3)+"s";
-
-
-
-setTimeout(()=>{
-
-container.appendChild(heart);
-
-},i*500);
-
-
-
-
-heart.onclick=function(){
-
-
-score++;
-
-document.getElementById("score").innerHTML=
-
-`Collected Hearts : ${score}/14`;
-
-
-
-heart.remove();
-
-
-
-
-if(score===14){
-
-
-document.getElementById("game").hidden=true;
-
-
-document.getElementById("loading").hidden=false;
-
-
-
-setTimeout(()=>{
-
-
-document.getElementById("loading").hidden=true;
-
-
-document.getElementById("letter").hidden=false;
-
-
-},3000);
-
-
+function openLetter() {
+    document.getElementById("popup").style.display = "block";
 }
 
-
-}
-
-
-}
-
-
-}
-
-
-
-
-function openLetter(){
-
-document.getElementById("popup").style.display="block";
-
-}
-
-
-
-function closeLetter(){
-
-document.getElementById("popup").style.display="none";
-
+function closeLetter() {
+    document.getElementById("popup").style.display = "none";
 }
